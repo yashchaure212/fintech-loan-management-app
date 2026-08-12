febe82aafad4d56279333f110f77ae6696eb1e52
yashchaure212 <yashchaure212@gmail.com>
2026-08-13 03:31:37 +0530

    chore: prepare production deployment

--- server/src/app.js
+++ server/src/app.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://fintech-loan-management-app-nine.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
